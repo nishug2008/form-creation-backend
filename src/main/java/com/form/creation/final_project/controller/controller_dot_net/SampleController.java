fe6cb626981fff8984686eb081cc617dec175981
Nishu Gupta <nishug2008@gmail.com>
2025-09-29 15:23:43 +0530

    call dotnet api

---
+++ src/main/java/com/form/creation/final_project/controller/controller_dot_net/SampleController.java
@@ -0,0 +1,24 @@
+package com.form.creation.final_project.controller.controller_dot_net;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class SampleController {
+
+    private final String DOTNET_API_URL = "http://localhost:5022/api/books";
+
+    @GetMapping("/getBooks")
+    public String getBookFromDotNet() {
+        System.out.println("Get Book Controller");
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(DOTNET_API_URL, String.class);
+
+        return response.getBody();
+    }
+
+}
